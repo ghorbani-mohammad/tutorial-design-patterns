9b1a886776f1bc6da11b2b46e77c0ec232205612
Mohammad <mohammad.ghorbani@hotmail.com>
2022-12-06 20:40:58 +0330

    minor change

--- 03-sigleton.py
+++ 03-sigleton.py
@@ -30,10 +30,8 @@ class Singleton(metaclass=SingletonMeta):
 
 if __name__ == "__main__":
     # The client code.
-
     s1 = Singleton()
     s2 = Singleton()
-
     if id(s1) == id(s2):
         print("Singleton works, both variables contain the same instance.")
     else:
