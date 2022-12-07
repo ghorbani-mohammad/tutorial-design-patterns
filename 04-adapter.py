d0fe49dbaef31e88abcc3bc4d29d26418a36ce61
Mohammad <mohammad.ghorbani@hotmail.com>
2022-12-07 19:34:54 +0330

    add class Adaptee to adapter

--- 04-adapter.py
+++ 04-adapter.py
@@ -5,3 +5,14 @@ class Target:
 
     def request(self) -> str:
         return "Target: The default target's behavior."
+
+
+class Adaptee:
+    """
+    The Adaptee contains some useful behavior, but its interface is incompatible
+    with the existing client code. The Adaptee needs some adaptation before the
+    client code can use it.
+    """
+
+    def specific_request(self) -> str:
+        return ".eetpadA eht fo roivaheb laicepS"
