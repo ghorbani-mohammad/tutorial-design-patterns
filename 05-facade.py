0e657be833655f1dafda3d9ec0ec45be2bb1ba14
Mohammad <mohammad.ghorbani@hotmail.com>
2022-12-07 19:45:21 +0330

    add main-code to facade pattern

--- 05-facade.py
+++ 05-facade.py
@@ -73,3 +73,13 @@ def client_code(facade: Facade) -> None:
     """
 
     print(facade.operation(), end="")
+
+
+if __name__ == "__main__":
+    # The client code may have some of the subsystem's objects already created.
+    # In this case, it might be worthwhile to initialize the Facade with these
+    # objects instead of letting the Facade create new instances.
+    subsystem1 = Subsystem1()
+    subsystem2 = Subsystem2()
+    facade = Facade(subsystem1, subsystem2)
+    client_code(facade)
