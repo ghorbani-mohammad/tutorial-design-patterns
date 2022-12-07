cdf6479f0c82dd7c26a20f61b75c094eae494532
Mohammad <mohammad.ghorbani@hotmail.com>
2022-12-07 19:45:00 +0330

    add client-code to facade pattern

--- 05-facade.py
+++ 05-facade.py
@@ -62,3 +62,14 @@ class Facade:
         results.append(self._subsystem1.operation_n())
         results.append(self._subsystem2.operation_z())
         return "\n".join(results)
+
+
+def client_code(facade: Facade) -> None:
+    """
+    The client code works with complex subsystems through a simple interface
+    provided by the Facade. When a facade manages the lifecycle of the
+    subsystem, the client might not even know about the existence of the
+    subsystem. This approach lets you keep the complexity under control.
+    """
+
+    print(facade.operation(), end="")
