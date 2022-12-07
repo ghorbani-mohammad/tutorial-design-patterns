9fd13ac3402279d4c75f09fb433f8f53b82e2096
Mohammad <mohammad.ghorbani@hotmail.com>
2022-12-07 19:35:40 +0330

    add Adapter class to adapter pattern

--- 04-adapter.py
+++ 04-adapter.py
@@ -16,3 +16,13 @@ class Adaptee:
 
     def specific_request(self) -> str:
         return ".eetpadA eht fo roivaheb laicepS"
+
+
+class Adapter(Target, Adaptee):
+    """
+    The Adapter makes the Adaptee's interface compatible with the Target's
+    interface via multiple inheritance.
+    """
+
+    def request(self) -> str:
+        return f"Adapter: (TRANSLATED) {self.specific_request()[::-1]}"
