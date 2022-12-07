e982134da7d9933aac9acd73718b4168830843ac
Mohammad <mohammad.ghorbani@hotmail.com>
2022-12-07 19:43:48 +0330

    add Subsystem2 class to facade pattern

--- 05-facade.py
+++ 05-facade.py
@@ -12,3 +12,17 @@ class Subsystem1:
 
     def operation_n(self) -> str:
         return "Subsystem1: Go!"
+
+
+class Subsystem2:
+    """
+    Some facades can work with multiple subsystems at the same time.
+    """
+
+    def operation1(self) -> str:
+        return "Subsystem2: Get ready!"
+
+    # ...
+
+    def operation_z(self) -> str:
+        return "Subsystem2: Fire!"
